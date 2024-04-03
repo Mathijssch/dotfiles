795fcb5184d4636221479fe533cc33f55063e186
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2024-04-03 13:16:08 +0200

    Set up notes for GET

--- home/shell/shared/notes.sh
+++ home/shell/shared/notes.sh
@@ -1,19 +1,33 @@
 export NOTES_DIR=~/Work/notebook/notes
 export NOTES_OUTDIR="${NOTES_DIR}_out"
+export NOTES_DIR_GET=~/Work/Research/GET/GET-notes/notes
+
+function open_notes() {
+    cd $1; nvim .
+}
+
+function notes_get {
+    open_notes $NOTES_DIR_GET
+}
 
 function notes {
-    cd $NOTES_DIR; nvim . 
+    open_notes $NOTES_DIR
 }
 
 function notes_old {
-    cd ~/obs-notes/Notebook; nvim .
+    open_notes ~/obs-notes/Notebook
 }
 
 
-function sketchwatch {
+function sketchwatch() {
     cd "~/utilities/sketchwatch"
     . actie 
-    python autocopy.py -i ~/Dropbox/Sketches -o $NOTES_DIR/Attachments
+    if [ $1 == "get" ]; then 
+        output=$NOTES_DIR_GET
+    else 
+        output=$NOTES_DIR
+    fi 
+    python autocopy.py -i ~/Dropbox/Sketches -o $output/Attachments
 }
 
 function servenotes {
@@ -22,5 +36,12 @@ function servenotes {
   live-server $NOTES_OUTDIR #& server_pid=$!
 
   kill $oxidian_pid
-  #kill $server_pid
+}
+
+function servenotes_get {
+  export RUST_LOG=info
+  oxidian watch $NOTES_DIR_GET & oxidian_pid=$!
+  live-server "${NOTES_DIR_GET}_out" #& server_pid=$!
+
+  kill $oxidian_pid
 }
