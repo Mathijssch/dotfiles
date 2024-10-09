2292c8138df546310c8e146e6160539ca2c95bab
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2024-10-09 10:01:52 +0200

    Install also creates symlinks to config

--- install.py
+++ install.py
@@ -3,6 +3,7 @@ import os
 import warnings
 import difflib
 HOME_DIR = "home"
+CONFIG_DIR = ".config"
 EXCLUDE_PATH = os.path.join(HOME_DIR, ".exclude")
 DOTFILES = "DOTFILES"
 
@@ -50,22 +51,45 @@ def set_path():
         f.write(new_line)
 
 
-def main():
+def process_existing(link, stats) -> bool:
+    if not os.path.exists(link):
+        return True
+    print(f"File {link} already exists.")
+    backup_file = link + "_BAK"
+    response = input(f"Want to overwrite it? A backup of the current version will be stored as {backup_file} [y/N]")
+    if response == "y":
+        os.rename(link, backup_file)
+        return True
+    return False
+
+
+def create_link_safely(link, file, stats):
+    if not process_existing(link, stats):
+        stats["already existing"] += 1
+        return False
+    print(f"Creating symlink {link} -> {file}")
+    isdir = os.path.isdir(file)
+    file = os.path.abspath(file)
+    os.symlink(file, link, target_is_directory=isdir)
+    stats["links created"] += 1
+
+
+def create_links_to_home():
+    exclude_list = get_exclude_list()
+
     stats = {"files detected": 0,
              "files ignored": 0,
              "links created": 0,
              "not a file": 0,
              "already existing": 0
              }
-    exclude_list = get_exclude_list()
-    set_path()
     for fileref in pathlib.Path(".").glob(f"{HOME_DIR}/*"):
         file = str(fileref)
         print(f"Processing {file}")
         stats["files detected"] += 1
         if excluded(exclude_list, file):
             print(f"Excluding {file}")
-            stats["ignored"] += 1
+            stats["files ignored"] += 1
             continue
         file = os.path.abspath(file)
         if not os.path.isfile(file):
@@ -74,25 +98,42 @@ def main():
             continue
 
         link = os.path.expanduser((os.path.join("~", os.path.split(file)[1])))
-        if os.path.isfile(link) or os.path.islink(link):
-            print(f"File {link} already exists. Skipping.")
-            backup_file = link + "_BAK"
-            response = input(f"Want to overwrite it? A backup of the current version will be stored as {backup_file} [y/N]")
-            if response == "y":
-                os.rename(link, backup_file)
-            else:
-                stats["already existing"] += 1
-                continue
-        print(f"Creating symlink {link} -> {file}")
-        os.symlink(file, link)
-        stats["links created"] += 1
+        create_link_safely(link, file, stats)
+    _print_stats(stats)
 
+
+def _print_stats(stats):
     print("-" * 80)
     for key, value in stats.items():
         print(f"{key:20s} | {value:5d}")
     print("-" * 80)
 
 
-if __name__ == "__main__":
+def create_links_to_configs():
+    configs = pathlib.Path(".").joinpath(CONFIG_DIR)
+    stats = {"configs detected": 0,
+             "links created": 0,
+             "skipped": 0,
+             "already existing": 0
+             }
 
+    for config_dir in os.listdir(configs):
+        config_dir = os.path.join(CONFIG_DIR, config_dir)
+        stats["configs detected"] += 1
+        response = input(f"Create link to config '{config_dir}' in the home directory? [Y/n]")
+        if response.lower() == "n":
+            stats["skipped"] += 1
+            continue
+        link = os.path.expanduser((os.path.join("~", CONFIG_DIR, os.path.split(config_dir)[1])))
+        create_link_safely(link, config_dir, stats)
+    _print_stats(stats)
+
+
+def main():
+    set_path()
+    create_links_to_home()
+    create_links_to_configs()
+
+
+if __name__ == "__main__":
     main()
